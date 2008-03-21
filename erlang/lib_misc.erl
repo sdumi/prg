db89914b75503eaaf0be020f52cd2213e293601d
Dumitru Sipos <dumitru.sipos@alcatel-lucent.ro>
2008-03-21 13:11:24 +0200

    added an inefficient variant of qsort (but very easy to implement and follow)

--- erlang/lib_misc.erl
+++ erlang/lib_misc.erl
@@ -1,5 +1,15 @@
 -module(lib_misc).
--export([for/3]).
+-export([for/3, qsort/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F)   -> [F(I)|for(I+1, Max, F)].
+
+qsort([]) -> [];
+qsort([Pivot|T]) ->
+    qsort([X || X <- T, X <  Pivot])
+    ++ [Pivot] ++
+    qsort([X || X <- T, X >= Pivot]).
+		 
+
+	       
+    
