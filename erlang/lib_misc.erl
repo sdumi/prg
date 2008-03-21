524b88e3642d71a64861382dbdd093aa0c4bff2c
Dumitru Sipos <dumitru.sipos@alcatel-lucent.ro>
2008-03-21 13:20:31 +0200

    added pythag/1. Again: it's too simple.

--- erlang/lib_misc.erl
+++ erlang/lib_misc.erl
@@ -1,15 +1,24 @@
 -module(lib_misc).
--export([for/3, qsort/1]).
+-export([for/3, qsort/1, pythag/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F)   -> [F(I)|for(I+1, Max, F)].
 
+%amazing how simple it seems to be
 qsort([]) -> [];
 qsort([Pivot|T]) ->
     qsort([X || X <- T, X <  Pivot])
     ++ [Pivot] ++
     qsort([X || X <- T, X >= Pivot]).
-		 
 
-	       
-    
+% the function pythag(N) generates a list of all integers {A, B, C}
+% such that  A^2 + B^2 = C^2 and where the sum of the sides is 
+% less than or equal to N
+pythag(N) ->
+    [{A, B, C} ||
+	A <- lists:seq(1, N),
+	B <- lists:seq(1, N),
+	C <- lists:seq(1, N),
+	A+B+C =< N,
+	A*A + B*B =:= C*C
+     ].
