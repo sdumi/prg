7852060059605b70dcf4ce2ea6981dd1844976fa
Dumitru Sipos <dumitru.sipos@alcatel-lucent.com>
2010-05-28 18:09:44 +0300

    a working solution it does fail on line with first 7 prime numbers from p001.txt: we reach recursion limit in that case... we need to introduce a test to see if the smaller number is a prime number or not

--- python/p001.py
+++ python/p001.py
@@ -14,3 +14,34 @@ numbers
     ex:
     ((4 6 8), 2, 24)
 """
+
+import sys
+
+def gcd(a, b):
+    """
+    we will use the euclidean algorithm
+    """
+    print a, b
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    if a == 1 or b == 1:
+        return 1
+    # make sure that a >= b:
+    (a, b) = (b, a) if (a < b) else (a, b)
+    return gcd (a-b, b)
+
+def lcm(a, b):
+    return (a*b)/gcd(a,b)
+
+f = sys.stdin
+lines = f.readlines()
+for line in lines:
+    numbers = tuple(map(int, line.split(' ')))
+    d = reduce(gcd, numbers)
+    m = reduce(lcm, numbers)
+    res = (numbers, d, m)
+    print res
+
+
