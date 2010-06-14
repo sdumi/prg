fc12840f568cddcde8c2392eb121a309769702a1
Dumitru Sipos <dumitru.sipos@gmail.com>
2010-06-14 20:00:19 +0300

    Automatic backup: new file version found.

--- web/django/mysite/urls.py
+++ web/django/mysite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-    (r'^latest/$', mysite.books.views.latest_books),
+    (r'^admin/', include(admin.site.urls)),
+    #(r'^latest/$', mysite.books.views.latest_books),
 )
